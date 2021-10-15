1b1a3269ac306cfeef9ed18f48a1cf8ecbebdf26
KimVP <KimVP@DOM201.LOCAL>
2021-10-15 12:11:32 +0700

    Initial commit

---
+++ app/src/test/java/com/example/kim_151021_lab_06_01_main/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.kim_151021_lab_06_01_main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
